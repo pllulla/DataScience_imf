a91997ec87653fe748b485a2c042e01cc8e68d30
pllulla <pllull.alles@gmail.com>
2023-06-05 17:51:28 +0200

    Primer push
    Primer push con todo el material

---
+++ 1-PRIMER MÓDULO/módulo1/R/Fundamentos R/4_Fundamentos_R/4_1_Primer_script.R
@@ -0,0 +1,26 @@
+a <- 5
+b <- 2.0
+c <- '5'
+
+# Comentario de una línea
+
+"
+Comentario de varias líneas
+de
+código"
+
+z <- 3
+
+# NOTA: Opción de comentar y descomentar varias líneas de código
+# 1. Selección de las líneas
+# 2. Sobre las líneas seleccionadas: CNTRL + SHIFT + C
+
+# Prueba a comentar/descomentar el siguiente código
+
+funcion.suma <- function(valor_a, valor_b){
+
+
+  suma <- valor_a + valor_b
+
+  return (suma)
+}
